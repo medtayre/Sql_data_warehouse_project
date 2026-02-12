4f63de78950a198dab1bbbfb92982ab6785a7b85
medtayre <122076849+medtayre@users.noreply.github.com>
2026-02-12 21:03:00 +0100

    Update inti_database.sql

--- Scripts/inti_database.sql
+++ Scripts/inti_database.sql
@@ -1,4 +1,19 @@
- Use master;         --             Swith to master 
+/*
+======================================================================================================
+ Create Database and Schemas
+======================================================================================================
+ Script Purpose 
+       This script creates a new database named 'DataWarehouse' after checking if it already exists.
+       if the database exists, it is dropped and recreated. Additionally, the Script sets up three Schemas 
+       Within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+       Running this script will drop the entire 'DataWarehouse' database if it exists.
+       All data in the database will be prementaly deleted. Proceed with Caution
+       and ensure you have proper backups before running this script.
+*/
+
+Use master;         --             Swith to master 
    GO
  
  -- Drop and recreate the 'DataWarehouse' database
@@ -15,10 +30,12 @@ GO
 
  USE DataWerhouse; 
  GO
+  
+-- Create Schemas 
  CREATE SCHEMA bronze;      -- Create the first Schema bronze
  GO
+  
  CREATE SCHEMA silver;      -- Create the Second Schema Silver
-
  GO                         --  Go: Separate batches when working with multiple SQL statements
 
  CREATE SCHEMA gold;        -- Create the thired Schema Gold
